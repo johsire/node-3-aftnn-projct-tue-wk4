7a1638be76ffce4ba5d44000d2603ac151df5c5d
Josiah Maina <josiah5115@gmail.com>
2018-07-17 15:52:39 -0600

    require our middleware in server.js file & use app.use to add it

--- server/server.js
+++ server/server.js
@@ -3,8 +3,7 @@ const express = require('express');
 const session = require('express-session');
 require('dotenv').config();
 
-// access our port in .env file;
-// const { SERVER_PORT } = process.env;
+const checkForSession = require('./middlewares/checkForSession');
 
 // create express app;
 const app = express();
@@ -18,6 +17,11 @@ app.use(session({
 })
 );
 
+// we use app.use to add checkForSession
+app.use(checkForSession);
+
+// access our port in .env file;
+// const { SERVER_PORT } = process.env;
 const SERVER_PORT = process.env.SERVER_PORT || 300;
 app.listen(SERVER_PORT, () => {
   console.log(`Kevin Hart - Its about to go down on port: ${SERVER_PORT}`);
