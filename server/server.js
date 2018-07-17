16cf5a5440e85e8b5add806828567286fa5fe7e5
Josiah Maina <josiah5115@gmail.com>
2018-07-17 15:11:08 -0600

    imported

--- server/server.js
+++ server/server.js
@@ -2,3 +2,19 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const session = require('express-session');
 require('dotenv').config();
+
+// access our port in .env file;
+const { SERVER_PORT } = process.env;
+
+// create express app;
+const app = express();
+
+// top level middleware -(global) 
+app.use(bodyParser.json());
+app.use(session({
+  secret: toptopsecret,
+  reave:fals,
+  saveUninitialized: false
+})
+)
+;
