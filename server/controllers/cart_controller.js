641b0f9064b9803486232315a92ca5c44498967e
Josiah Maina <josiah5115@gmail.com>
2018-07-17 18:54:36 -0600

    add obj created

--- server/controllers/cart_controller.js
+++ server/controllers/cart_controller.js
@@ -1,17 +1,28 @@
 const swag = require('../models/swag');
-import { checkout } from '../../src/ducks/reducer';
 
 module.exports = {
-  
-  add: (req, res, next) => {
+  add: ( req, res, next ) => {
+    const { id } = req.query;
+    let { cart } = req.session.user;
+
+    // This will return -1 if it isn't in the cart:
+    const index = cart.findIndex(swag => swag.id === id);
+
+    if(index === -1) {
+      const selectedSwag = swag.find(swag => swag.id === id);
+
+      cart.push(selectedSwag);
+      req.session.user.total += selectedSwag.price;
+    }
+
+    res.status(200).send(req.session.user);
+  },
+
+  delete: ( req, res, next ) => {
 
   },
 
-  delete: (req, res, next) => {
+  checkout: ( req, res, next ) => {
 
- },
-
-  checkout: (req, res, next) => {
-
- }
-}
+  }
+} 
